09cc9b6dab07f5cdac0ddb2b4325f27656cc32ba
Aria Stewart <aredridel@dinhe.net>
2023-09-02 15:05:26 -0400

    updater works

--- functions.php
+++ functions.php
@@ -73,162 +73,151 @@ function gg2023_post_footer() {
 	}
 }
 
-if( ! class_exists( 'mishaUpdateChecker' ) ) {
+class GG2023ThemeUpdateChecker {
 
-	class mishaUpdateChecker{
+	public $theme_slug;
+	public $version;
+	public $cache_key;
+	public $cache_allowed;
 
-		public $theme_slug;
-		public $version;
-		public $cache_key;
-		public $cache_allowed;
+	public function __construct() {
 
-		public function __construct() {
+		$this->theme_slug = 'gallowglas-2023';
+		$this->version = '1.0';
+		$this->cache_key = 'gg2023_custom_update';
+		$this->cache_allowed = false;
 
-			$this->theme_slug = theme_basename( __DIR__ );
-			$this->version = '1.0';
-			$this->cache_key = 'misha_custom_upd';
-			$this->cache_allowed = false;
-
-			add_filter( 'themes_api', array( $this, 'info' ), 20, 3 );
-			add_filter( 'site_transient_update_themes', array( $this, 'update' ) );
-			add_action( 'upgrader_process_complete', array( $this, 'purge' ), 10, 2 );
-
-		}
-
-		public function request(){
-
-			$remote = get_transient( $this->cache_key );
-
-			if( false === $remote || ! $this->cache_allowed ) {
-
-				$remote = wp_remote_get(
-					'https://aredridel.github.io/gallowglas-2023-theme/info.json',
-					array(
-						'timeout' => 10,
-						'headers' => array(
-							'Accept' => 'application/json'
-						)
-					)
-				);
-
-				if(
-					is_wp_error( $remote )
-					|| 200 !== wp_remote_retrieve_response_code( $remote )
-					|| empty( wp_remote_retrieve_body( $remote ) )
-				) {
-					return false;
-				}
-
-				set_transient( $this->cache_key, $remote, DAY_IN_SECONDS );
-
-			}
-
-			$remote = json_decode( wp_remote_retrieve_body( $remote ) );
-
-			return $remote;
-
-		}
-
-
-		function info( $res, $action, $args ) {
-
-			// print_r( $action );
-			// print_r( $args );
-
-			// do nothing if you're not getting theme information right now
-			if( 'theme_information' !== $action ) {
-				return $res;
-			}
-
-			// do nothing if it is not our theme
-			if( $this->theme_slug !== $args->slug ) {
-				return $res;
-			}
-
-			// get updates
-			$remote = $this->request();
-
-			if( ! $remote ) {
-				return $res;
-			}
-
-			$res = new stdClass();
-
-			$res->name = $remote->name;
-			$res->slug = $remote->slug;
-			$res->version = $remote->version;
-			$res->tested = $remote->tested;
-			$res->requires = $remote->requires;
-			$res->author = $remote->author;
-			$res->author_profile = $remote->author_profile;
-			$res->download_link = $remote->download_url;
-			$res->trunk = $remote->download_url;
-			$res->requires_php = $remote->requires_php;
-			$res->last_updated = $remote->last_updated;
-
-			$res->sections = array(
-				'description' => $remote->sections->description,
-				'installation' => $remote->sections->installation,
-				'changelog' => $remote->sections->changelog
-			);
-
-			if( ! empty( $remote->banners ) ) {
-				$res->banners = array(
-					'low' => $remote->banners->low,
-					'high' => $remote->banners->high
-				);
-			}
-
-			return $res;
-
-		}
-
-		public function update( $transient ) {
-
-			if ( empty($transient->checked ) ) {
-				return $transient;
-			}
-
-			$remote = $this->request();
-
-			if(
-				$remote
-				&& version_compare( $this->version, $remote->version, '<' )
-				&& version_compare( $remote->requires, get_bloginfo( 'version' ), '<=' )
-				&& version_compare( $remote->requires_php, PHP_VERSION, '<' )
-			) {
-				$res = new stdClass();
-				$res->slug = $this->theme_slug;
-				$res->theme = theme_basename( __FILE__ );
-				$res->new_version = $remote->version;
-				$res->tested = $remote->tested;
-				$res->package = $remote->download_url;
-
-				$transient->response[ $res->theme ] = $res;
-
-	    }
-
-			return $transient;
-
-		}
-
-		public function purge( $upgrader, $options ){
-
-			if (
-				$this->cache_allowed
-				&& 'update' === $options['action']
-				&& 'theme' === $options[ 'type' ]
-			) {
-				// just clean the cache when new theme version is installed
-				delete_transient( $this->cache_key );
-			}
-
-		}
-
+		add_filter( 'themes_api', array( $this, 'info' ), 20, 3 );
+		add_filter( 'site_transient_update_themes', array( $this, 'update' ) );
+		add_action( 'upgrader_process_complete', array( $this, 'purge' ), 10, 2 );
 
 	}
 
-	new mishaUpdateChecker();
+	public function request(){
+
+		$remote = get_transient( $this->cache_key );
+
+		if( false === $remote || ! $this->cache_allowed ) {
+
+			$remote = wp_remote_get(
+				'https://aredridel.github.io/gallowglas-2023-theme/info.json',
+				array(
+					'timeout' => 10,
+					'headers' => array(
+						'Accept' => 'application/json'
+					)
+				)
+			);
+
+			if(
+				is_wp_error( $remote )
+				|| 200 !== wp_remote_retrieve_response_code( $remote )
+				|| empty( wp_remote_retrieve_body( $remote ) )
+			) {
+				return false;
+			}
+
+			set_transient( $this->cache_key, $remote, DAY_IN_SECONDS );
+
+		}
+
+		$remote = json_decode( wp_remote_retrieve_body( $remote ) );
+
+		return $remote;
+
+	}
+
+
+	function info( $res, $action, $args ) {
+
+		// print_r( $action );
+		// print_r( $args );
+
+		// do nothing if you're not getting theme information right now
+		if( 'theme_information' !== $action ) {
+			return $res;
+		}
+
+		// do nothing if it is not our theme
+		if( $this->theme_slug !== $args->slug ) {
+			return $res;
+		}
+
+		// get updates
+		$remote = $this->request();
+
+		if( ! $remote ) {
+			return $res;
+		}
+
+		$res = [
+			"name" => $remote->name,
+			"slug" => $remote->slug,
+			"version" => $remote->version,
+			"tested" => $remote->tested,
+			"requires" => $remote->requires,
+			"author" => $remote->author,
+			"author_profile" => $remote->author_profile,
+			"download_link" => $remote->download_url,
+			"trunk" => $remote->download_url,
+			"requires_php" => $remote->requires_php,
+			"last_updated" => $remote->last_updated,
+			'sections' => [
+				'description' => $remote->sections->description,
+				'installation' => $remote->sections->installation,
+				'changelog' => $remote->sections->changelog
+			]
+		];
+
+		return $res;
+
+	}
+
+	public function update( $transient ) {
+
+		if ( empty($transient->checked ) ) {
+			return $transient;
+		}
+
+		$remote = $this->request();
+
+		if(
+			$remote
+			&& version_compare( $this->version, $remote->version, '<' )
+			&& version_compare( $remote->requires, get_bloginfo( 'version' ), '<=' )
+			&& version_compare( $remote->requires_php, PHP_VERSION, '<' )
+		) {
+			$res = [
+				"slug" => $this->theme_slug,
+				"theme" => 'gallowglas-2023',
+				"new_version" => $remote->version,
+				"tested" => $remote->tested,
+				"package" => $remote->download_url
+			];
+
+			$transient->response[ $res['theme'] ] = $res;
+
+	}
+
+		return $transient;
+
+	}
+
+	public function purge( $upgrader, $options ){
+
+		if (
+			$this->cache_allowed
+			&& 'update' === $options['action']
+			&& 'theme' === $options[ 'type' ]
+		) {
+			// just clean the cache when new theme version is installed
+			delete_transient( $this->cache_key );
+		}
+
+	}
+
 
 }
+
+new GG2023ThemeUpdateChecker();
 
