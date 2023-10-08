096138181aa16684da3d77ede17aaa668d23347c
Aria Stewart <aredridel@dinhe.net>
2023-10-08 16:18:33 -0400

    MAke archive pages work

--- loop.php
+++ loop.php
@@ -18,6 +18,11 @@
 					<div class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentyten' ) . '</span> %title' ); ?></div>
 					<div class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentyten' ) . '</span>' ); ?></div>
 				</div>
+			<?php elseif ( $wp_query->max_num_pages > 1 ) : ?>
+				<div id="nav-below" class="navigation">
+					<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'gg2023' ) ); ?></div>
+					<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'gg2023' ) ); ?></div>
+				</div>
 			<?php endif ?>
 
 		<?php endwhile ?>
